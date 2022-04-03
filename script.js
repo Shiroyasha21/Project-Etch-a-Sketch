a1629a8360e30ae89bd29edceeb0add0e69e9ac3
Azkieel <red.ezequiel@gmail.com>
2022-04-03 21:26:09 +0800

    writes buttons and erase feature. figuring out the canvas structure code

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const project = document.querySelector('project');
 const projectContainer = document.querySelector('#projectContainer');
+const tools = document.querySelector('#tools');
 
 const canvas = document.createElement('div');
-canvas.className = 'canvas';
+canvas.setAttribute('id', 'canvas');
 projectContainer.append(canvas);
 
+const valueTextField = document.createElement('input');
+valueTextField.setAttribute('type', 'number');
+valueTextField.setAttribute('min', '1');
+valueTextField.setAttribute('max', '100');
+valueTextField.setAttribute('value', '3');
+valueTextField.className = 'textField';
+tools.append(valueTextField);
 
-function gridMaker(col, row) {
-    for (i = 0; i < col; i++)     {
+
+let gridValue = valueTextField.value;
+
+function gridMaker(gridValue) {
+    for (i = 0; i < gridValue; i++)     {
         const column = document.createElement('div');
         column.className = 'columnBox';
         canvas.append(column);
-        for (j = 0; j < row; j++) {
+        for (j = 0; j < gridValue; j++) {
             const row = document.createElement('div');
             row.className = 'rowBox';
             column.append(row);
@@ -19,10 +30,38 @@ function gridMaker(col, row) {
     }
 }
 
-gridMaker(3, 3);
+// valueTextField.addEventListener('keydown', registerValueEntered);
 
-var eraser = document.getElementById('#erasure');
-let elementSketch = document.querySelector('canvas');
+// function registerValueEntered(event) {
+//     if (event.which === 13) {
+//         valueRegister();
+//     }
+// }
 
-console.log(eraser);
-console.log(elementSketch);
+
+gridMaker(gridValue);
+
+
+valueTextField.oninput = () => {
+    if (valueTextField.value > 100) {
+        valueTextField.value = 100;
+    } else if (valueTextField.value.length > 3) {
+        valueTextField.value = valueTextField.value.slice(0, 3);
+    }
+}
+
+
+
+
+
+
+
+var eraser = document.getElementById('erasure');
+let elementSketch = document.getElementById('canvas');
+
+eraser.addEventListener('click', e => {
+    elementSketch.remove();
+})
+
+
+
